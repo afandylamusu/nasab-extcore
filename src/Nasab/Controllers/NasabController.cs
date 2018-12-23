623ed2961e7afb16dbfaf6d96afabdf1ff35540c
afandylamusu <afandy.lamusu@live.co.uk>
2018-12-23 18:01:00 +0700

    add nasab controller

---
+++ src/Nasab/Controllers/NasabController.cs
@@ -0,0 +1,17 @@
+﻿using ExtCore.Data.Abstractions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Nasab.Controllers
+{
+    public class NasabController : Barebone.Controllers.ControllerBase
+    {
+        public NasabController(IStorage storage) : base(storage)
+        {
+        }
+
+        public ActionResult Index()
+        {
+            return this.View();
+        }
+    }
+}
